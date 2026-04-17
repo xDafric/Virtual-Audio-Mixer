7ebbee33c464429e1d154c15245b197e2152e010
Dafric <dafricyt@gmail.com>
2026-04-17 23:11:39 +0200

    fix: add npm author, fix main.ts type error

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { app, BrowserWindow, ipcMain } from "electron";
 import path from "node:path";
 import started from "electron-squirrel-startup";
 
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
+declare const MAIN_WINDOW_VITE_NAME: string;
+
 const {
   getPlaybackDevices,
   getCaptureDevices,
